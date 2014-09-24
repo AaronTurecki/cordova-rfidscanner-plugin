dd13a94ce896d3e0722d72321d3753e1946f0b8b
Aaron Turecki <aaronturecki@gmail.com>
2014-09-23 19:09:47 -0700

    alsdfjl

--- src/android/Scan.java
+++ src/android/Scan.java
@@ -1,8 +1,10 @@
 package com.assettagz.cordova.plugin.scan;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
