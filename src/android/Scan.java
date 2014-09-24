4224fd057017df742c49f990f38c704b41c6c412
Aaron Turecki <aaronturecki@gmail.com>
2014-09-23 19:21:36 -0700

    alsdjf

--- src/android/Scan.java
+++ src/android/Scan.java
@@ -155,7 +155,7 @@ public class Scan extends CordovaPlugin implements iRcpEvent2,
             		
             		PluginResult result = new PluginResult(PluginResult.Status.OK, dataText);
               		result.setKeepCallback(false);
-              		if (connectionCallbackContext != null) {
+              		if (callbackContext != null) {
               			callbackContext.sendPluginResult(result);
               		}
             	}
